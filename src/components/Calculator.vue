b2ed1d296ef56e5b9b552dec248a0ecb7e406827
Kelvin Wong <kelvinwonggg@gmail.com>
2020-08-07 00:22:59 +0800

    c-3

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <h1>Calculator</h1>
+    <label>Price:</label>
+    <input
+      :value="price"
+    />
+    <br>
+    <label>Rate:</label>
+    <input
+      :value="rate"
+    />
+    <br>
+    <label>Discount:</label>
+    <input
+      :value="discount"
+    />
   </div>
 </template>
 
