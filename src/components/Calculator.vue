38076186f0c67eb6234febe22a832ebe3d4a06ae
Kelvin Wong <kelvinwonggg@gmail.com>
2020-08-07 01:24:17 +0800

    e

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -36,6 +36,41 @@ export default {
       discountNumber: this.getNumber(this.discount),
     }
   },
+  watch: {
+    priceNumber(newPrice) {
+      // calculate discountNumber
+      let newDiscount = this.getNumber(newPrice * this.rateNumber / 100)
+      this.discountNumber = newDiscount
+      
+      // update currency display
+      this.discountDisplay = this.getCurrency(newDiscount)
+
+      // emit change
+      this.$emit('update:discount', newDiscount)
+    },
+    rateNumber(newRate) {
+      // calculate discountNumber
+      let newDiscount = this.getNumber(this.priceNumber * newRate / 100)
+      this.discountNumber = newDiscount
+      
+      // update currency display
+      this.discountDisplay = this.getCurrency(newDiscount)
+
+      // emit change
+      this.$emit('update:discount', newDiscount)
+    },
+    discountNumber(newDiscount) {
+      // calculate rateNumber
+      let newRate = this.getNumber(newDiscount / this.priceNumber * 100)
+      this.rateNumber = newRate
+
+      // update currency display
+      this.rateDisplay = this.getCurrency(newRate)
+
+      // emit change
+      this.$emit('update:rate', newRate)
+    }
+  },
   methods: {
     getNumber(string) {
       const numberString = numeral(string).format('0,0.00')
